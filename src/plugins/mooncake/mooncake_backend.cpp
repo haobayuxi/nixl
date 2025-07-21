455e5c1ac3481b9447756cf4b6a25141ec0c4c52
haobayuxi <whaohit@gmail.com>
2025-07-21 02:13:04 +0000

    clang format

--- src/plugins/mooncake/mooncake_backend.cpp
+++ src/plugins/mooncake/mooncake_backend.cpp
@@ -25,11 +25,12 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-std::vector<std::string> findLocalIpAddresses() {
+std::vector<std::string>
+findLocalIpAddresses() {
     std::vector<std::string> ips;
     struct ifaddrs *ifaddr, *ifa;
 
-    if (getifaddrs(&ifaddr) == -1) {
+    if (getifaddrs (&ifaddr) == -1) {
         return ips;
     }
 
@@ -39,45 +40,49 @@ std::vector<std::string> findLocalIpAddresses() {
         }
 
         if (ifa->ifa_addr->sa_family == AF_INET) {
-            if (strcmp(ifa->ifa_name, "lo") == 0) {
+            if (strcmp (ifa->ifa_name, "lo") == 0) {
                 continue;
             }
 
             char host[NI_MAXHOST];
-            if (getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), host,
-                            NI_MAXHOST, nullptr, 0, NI_NUMERICHOST) == 0) {
-                ips.push_back(host);
+            if (getnameinfo (ifa->ifa_addr,
+                             sizeof (struct sockaddr_in),
+                             host,
+                             NI_MAXHOST,
+                             nullptr,
+                             0,
+                             NI_NUMERICHOST) == 0) {
+                ips.push_back (host);
             }
         }
     }
 
-    freeifaddrs(ifaddr);
+    freeifaddrs (ifaddr);
     return ips;
 }
 
-nixlMooncakeEngine::nixlMooncakeEngine (const nixlBackendInitParams* init_params)
-: nixlBackendEngine (init_params) {
+nixlMooncakeEngine::nixlMooncakeEngine (const nixlBackendInitParams *init_params)
+    : nixlBackendEngine (init_params) {
     local_agent_name_ = init_params->localAgent;
     auto ips = findLocalIpAddresses();
     std::string segment_name = "127.0.0.1";
     if (!ips.empty()) segment_name = ips[0];
-    if (getenv("NIXL_MOONCAKE_IP_ADDR"))
-        segment_name = std::string(getenv("NIXL_MOONCAKE_IP_ADDR"));
-    engine_ = createTransferEngine("P2PHANDSHAKE",
-                                   segment_name.c_str(),
-                                   "", 0, true);
+    if (getenv ("NIXL_MOONCAKE_IP_ADDR"))
+        segment_name = std::string (getenv ("NIXL_MOONCAKE_IP_ADDR"));
+    engine_ = createTransferEngine ("P2PHANDSHAKE", segment_name.c_str(), "", 0, true);
 }
 
-nixl_mem_list_t nixlMooncakeEngine::getSupportedMems () const {
+nixl_mem_list_t
+nixlMooncakeEngine::getSupportedMems() const {
     nixl_mem_list_t mems;
-    mems.push_back(DRAM_SEG);
-    mems.push_back(VRAM_SEG);
+    mems.push_back (DRAM_SEG);
+    mems.push_back (VRAM_SEG);
     return mems;
 }
 
 // Through parent destructor the unregister will be called.
-nixlMooncakeEngine::~nixlMooncakeEngine () {
-    destroyTransferEngine(engine_);
+nixlMooncakeEngine::~nixlMooncakeEngine() {
+    destroyTransferEngine (engine_);
 }
 
 // TODO We purposely set this function as empty.
@@ -89,57 +94,60 @@ nixlMooncakeEngine::~nixlMooncakeEngine () {
 // (segment name in the context of Mooncake Transfer Engine).
 // loadRemoteConnInfo() opens the segment, which implicitly retrieves metadata
 // (such as QP numbers) of the remote agent.
-nixl_status_t nixlMooncakeEngine::connect(const std::string &remote_agent) {
+nixl_status_t
+nixlMooncakeEngine::connect (const std::string &remote_agent) {
     return NIXL_SUCCESS;
 }
 
 // TODO We purposely set this function as empty.
 // Will be changed to follow NIXL's paradigm after refactoring Mooncake Transfer Engine.
-nixl_status_t nixlMooncakeEngine::disconnect(const std::string &remote_agent) {
+nixl_status_t
+nixlMooncakeEngine::disconnect (const std::string &remote_agent) {
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::getConnInfo(std::string &str) const {
+nixl_status_t
+nixlMooncakeEngine::getConnInfo (std::string &str) const {
     const static size_t kBufLen = 64;
     char buf_out[kBufLen];
-    getLocalIpAndPort(engine_, buf_out, kBufLen);
+    getLocalIpAndPort (engine_, buf_out, kBufLen);
     str = buf_out;
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::loadRemoteConnInfo (const std::string &remote_agent,
-                                                      const std::string &remote_conn_info)
-{
-    std::lock_guard<std::mutex> lock(mutex_);
-    auto segment_id = openSegment(engine_, remote_conn_info.c_str());
+nixl_status_t
+nixlMooncakeEngine::loadRemoteConnInfo (const std::string &remote_agent,
+                                        const std::string &remote_conn_info) {
+    std::lock_guard<std::mutex> lock (mutex_);
+    auto segment_id = openSegment (engine_, remote_conn_info.c_str());
     if (segment_id < 0) return NIXL_ERR_BACKEND;
-        connected_agents_[remote_agent].segment_id = segment_id;
+    connected_agents_[remote_agent].segment_id = segment_id;
     return NIXL_SUCCESS;
 }
 
 struct nixlMooncakeBackendMD : public nixlBackendMD {
-    nixlMooncakeBackendMD(bool isPrivate) : nixlBackendMD(isPrivate) {}
-    virtual ~nixlMooncakeBackendMD(){}
+    nixlMooncakeBackendMD (bool isPrivate) : nixlBackendMD (isPrivate) {}
+    virtual ~nixlMooncakeBackendMD() {}
     void *addr;
     size_t length;
     int ref_cnt;
 };
 
-nixl_status_t nixlMooncakeEngine::registerMem (const nixlBlobDesc &mem,
-                                               const nixl_mem_t &nixl_mem,
-                                               nixlBackendMD* &out)
-{
-    std::lock_guard<std::mutex> lock(mutex_);
-    if (mem_reg_info_.count(mem.addr)) {
+nixl_status_t
+nixlMooncakeEngine::registerMem (const nixlBlobDesc &mem,
+                                 const nixl_mem_t &nixl_mem,
+                                 nixlBackendMD *&out) {
+    std::lock_guard<std::mutex> lock (mutex_);
+    if (mem_reg_info_.count (mem.addr)) {
         auto priv = mem_reg_info_[mem.addr];
         priv->ref_cnt++;
         out = priv;
         return NIXL_SUCCESS;
     }
-    int err = registerLocalMemory(engine_, (void *) mem.addr, mem.len, "*", 1);
+    int err = registerLocalMemory (engine_, (void *)mem.addr, mem.len, "*", 1);
     if (err) return NIXL_ERR_BACKEND;
-    auto priv = new nixlMooncakeBackendMD(true);
-    priv->addr = (void *) mem.addr;
+    auto priv = new nixlMooncakeBackendMD (true);
+    priv->addr = (void *)mem.addr;
     priv->length = mem.len;
     priv->ref_cnt = 1;
     out = priv;
@@ -147,14 +155,14 @@ nixl_status_t nixlMooncakeEngine::registerMem (const nixlBlobDesc &mem,
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::deregisterMem (nixlBackendMD* meta)
-{
-    std::lock_guard<std::mutex> lock(mutex_);
-    auto priv = (nixlMooncakeBackendMD *) meta;
+nixl_status_t
+nixlMooncakeEngine::deregisterMem (nixlBackendMD *meta) {
+    std::lock_guard<std::mutex> lock (mutex_);
+    auto priv = (nixlMooncakeBackendMD *)meta;
     priv->ref_cnt--;
     if (priv->ref_cnt) return NIXL_SUCCESS;
-    int err = unregisterLocalMemory(engine_, priv->addr);
-    mem_reg_info_.erase((uint64_t)priv->addr);
+    int err = unregisterLocalMemory (engine_, priv->addr);
+    mem_reg_info_.erase ((uint64_t)priv->addr);
     delete priv;
     return err == 0 ? NIXL_SUCCESS : NIXL_ERR_BACKEND;
 }
@@ -165,56 +173,53 @@ nixl_status_t nixlMooncakeEngine::deregisterMem (nixlBackendMD* meta)
 // Mooncake Transfer Engine exchanges metadata by itself without any explicit interface,
 // which is different from NIXL's paradigm.
 // Therefore no metadata needs to be exposed to the outside.
-nixl_status_t nixlMooncakeEngine::getPublicData (const nixlBackendMD* meta,
-                                                 std::string &str) const
-{
+nixl_status_t
+nixlMooncakeEngine::getPublicData (const nixlBackendMD *meta, std::string &str) const {
     return NIXL_SUCCESS;
 }
 
 // TODO We purposely set this function as empty.
 // Will be changed to follow NIXL's paradigm after refactoring Mooncake Transfer Engine.
 nixl_status_t
-nixlMooncakeEngine::loadLocalMD (nixlBackendMD* input,
-                                 nixlBackendMD* &output)
-{
+nixlMooncakeEngine::loadLocalMD (nixlBackendMD *input, nixlBackendMD *&output) {
     output = nullptr;
     return NIXL_SUCCESS;
 }
 
 // TODO We purposely set this function as empty.
 // Will be changed to follow NIXL's paradigm after refactoring Mooncake Transfer Engine.
-nixl_status_t nixlMooncakeEngine::loadRemoteMD (const nixlBlobDesc &input,
-                                                const nixl_mem_t &nixl_mem,
-                                                const std::string &remote_agent,
-                                                nixlBackendMD* &output)
-{
+nixl_status_t
+nixlMooncakeEngine::loadRemoteMD (const nixlBlobDesc &input,
+                                  const nixl_mem_t &nixl_mem,
+                                  const std::string &remote_agent,
+                                  nixlBackendMD *&output) {
     output = nullptr;
     return NIXL_SUCCESS;
 }
 
 // TODO We purposely set this function as empty.
 // Will be changed to follow NIXL's paradigm after refactoring Mooncake Transfer Engine.
-nixl_status_t nixlMooncakeEngine::unloadMD (nixlBackendMD* input)
-{
+nixl_status_t
+nixlMooncakeEngine::unloadMD (nixlBackendMD *input) {
     return NIXL_SUCCESS;
 }
 
 struct nixlMooncakeBackendReqH : public nixlBackendReqH {
     nixlMooncakeBackendReqH() : nixlBackendReqH() {}
-    virtual ~nixlMooncakeBackendReqH(){}
+    virtual ~nixlMooncakeBackendReqH() {}
     uint64_t batch_id;
     size_t request_count;
 };
 
-nixl_status_t nixlMooncakeEngine::prepXfer (const nixl_xfer_op_t &operation,
-                                            const nixl_meta_dlist_t &local,
-                                            const nixl_meta_dlist_t &remote,
-                                            const std::string &remote_agent,
-                                            nixlBackendReqH* &handle,
-                                            const nixl_opt_b_args_t* opt_args) const
-{
+nixl_status_t
+nixlMooncakeEngine::prepXfer (const nixl_xfer_op_t &operation,
+                              const nixl_meta_dlist_t &local,
+                              const nixl_meta_dlist_t &remote,
+                              const std::string &remote_agent,
+                              nixlBackendReqH *&handle,
+                              const nixl_opt_b_args_t *opt_args) const {
     const static size_t kMaxRequestCount = 1024;
-    uint64_t batch_id = allocateBatchID(engine_, kMaxRequestCount);
+    uint64_t batch_id = allocateBatchID (engine_, kMaxRequestCount);
     if (batch_id == INVALID_BATCH) return NIXL_ERR_BACKEND;
     auto priv = new nixlMooncakeBackendReqH();
     priv->batch_id = batch_id;
@@ -223,20 +228,19 @@ nixl_status_t nixlMooncakeEngine::prepXfer (const nixl_xfer_op_t &operation,
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::postXfer (const nixl_xfer_op_t &operation,
-                                            const nixl_meta_dlist_t &local,
-                                            const nixl_meta_dlist_t &remote,
-                                            const std::string &remote_agent,
-                                            nixlBackendReqH* &handle,
-                                            const nixl_opt_b_args_t* opt_args) const
-{
-    auto priv = (nixlMooncakeBackendReqH *) handle;
+nixl_status_t
+nixlMooncakeEngine::postXfer (const nixl_xfer_op_t &operation,
+                              const nixl_meta_dlist_t &local,
+                              const nixl_meta_dlist_t &remote,
+                              const std::string &remote_agent,
+                              nixlBackendReqH *&handle,
+                              const nixl_opt_b_args_t *opt_args) const {
+    auto priv = (nixlMooncakeBackendReqH *)handle;
     int segment_id;
     {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto agent = connected_agents_.find(remote_agent);
-        if (agent == connected_agents_.end())
-            return NIXL_ERR_INVALID_PARAM;
+        std::lock_guard<std::mutex> lock (mutex_);
+        const auto agent = connected_agents_.find (remote_agent);
+        if (agent == connected_agents_.end()) return NIXL_ERR_INVALID_PARAM;
         segment_id = agent->second.segment_id;
     }
     if (local.descCount() != remote.descCount()) return NIXL_ERR_INVALID_PARAM;
@@ -251,27 +255,27 @@ nixl_status_t nixlMooncakeEngine::postXfer (const nixl_xfer_op_t &operation,
         request[index].target_id = segment_id;
     }
     int rc = 0;
-    if (opt_args->hasNotif){
+    if (opt_args->hasNotif) {
         notify_msg_t notify_msg;
-        notify_msg.name = const_cast<char*>(local_agent_name_.c_str());
-        notify_msg.msg = const_cast<char*>(opt_args->notifMsg.c_str());
+        notify_msg.name = const_cast<char *> (local_agent_name_.c_str());
+        notify_msg.msg = const_cast<char *> (opt_args->notifMsg.c_str());
         rc = submitTransferWithNotify (engine_, priv->batch_id, request, request_count, notify_msg);
-    }else {
-        rc = submitTransfer(engine_, priv->batch_id, request, request_count);
+    } else {
+        rc = submitTransfer (engine_, priv->batch_id, request, request_count);
     }
-    delete []request;
+    delete[] request;
     if (rc) return NIXL_ERR_BACKEND;
     priv->request_count += request_count;
     return NIXL_IN_PROG;
 }
 
-nixl_status_t nixlMooncakeEngine::checkXfer (nixlBackendReqH* handle) const
-{
-    auto priv = (nixlMooncakeBackendReqH *) handle;
+nixl_status_t
+nixlMooncakeEngine::checkXfer (nixlBackendReqH *handle) const {
+    auto priv = (nixlMooncakeBackendReqH *)handle;
     bool has_failed = false;
     for (size_t index = 0; index < priv->request_count; ++index) {
         transfer_status_t status;
-        int rc = getTransferStatus(engine_, priv->batch_id, index, &status);
+        int rc = getTransferStatus (engine_, priv->batch_id, index, &status);
         if (rc || status.status == STATUS_FAILED)
             has_failed = true;
         else if (status.status == STATUS_PENDING || status.status == STATUS_WAITING)
@@ -280,39 +284,38 @@ nixl_status_t nixlMooncakeEngine::checkXfer (nixlBackendReqH* handle) const
     return has_failed ? NIXL_ERR_BACKEND : NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::releaseReqH(nixlBackendReqH* handle) const
-{
-    auto priv = (nixlMooncakeBackendReqH *) handle;
-    freeBatchID(engine_, priv->batch_id);
+nixl_status_t
+nixlMooncakeEngine::releaseReqH (nixlBackendReqH *handle) const {
+    auto priv = (nixlMooncakeBackendReqH *)handle;
+    freeBatchID (engine_, priv->batch_id);
     delete priv;
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::getNotifs(notif_list_t &notif_list){
-    if (notif_list.size()!=0)
-        return NIXL_ERR_INVALID_PARAM;
+nixl_status_t
+nixlMooncakeEngine::getNotifs (notif_list_t &notif_list) {
+    if (notif_list.size() != 0) return NIXL_ERR_INVALID_PARAM;
     int size = 0;
-    notify_msg_t* notify_msgs = getNotifsFromEngine(engine_, &size);
-    for(int i = 0;i < size;i++){
-        notif_list.push_back(std::make_pair(notify_msgs[i].name,notify_msgs[i].msg));
+    notify_msg_t *notify_msgs = getNotifsFromEngine (engine_, &size);
+    for (int i = 0; i < size; i++) {
+        notif_list.push_back (std::make_pair (notify_msgs[i].name, notify_msgs[i].msg));
     }
-    free(notify_msgs);
+    free (notify_msgs);
     return NIXL_SUCCESS;
 }
 
-nixl_status_t nixlMooncakeEngine::genNotif(const std::string &remote_agent, const std::string &msg) const
-{
+nixl_status_t
+nixlMooncakeEngine::genNotif (const std::string &remote_agent, const std::string &msg) const {
     int segment_id;
     {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto agent = connected_agents_.find(remote_agent);
-        if (agent == connected_agents_.end())
-            return NIXL_ERR_INVALID_PARAM;
+        std::lock_guard<std::mutex> lock (mutex_);
+        const auto agent = connected_agents_.find (remote_agent);
+        if (agent == connected_agents_.end()) return NIXL_ERR_INVALID_PARAM;
         segment_id = agent->second.segment_id;
     }
     notify_msg_t notify_msg;
-    notify_msg.name = const_cast<char*>(local_agent_name_.c_str());
-    notify_msg.msg = const_cast<char*>(msg.c_str());
-    int ret = genNotifyInEngine(engine_, segment_id, notify_msg);
-    return nixl_status_t(ret);
+    notify_msg.name = const_cast<char *> (local_agent_name_.c_str());
+    notify_msg.msg = const_cast<char *> (msg.c_str());
+    int ret = genNotifyInEngine (engine_, segment_id, notify_msg);
+    return nixl_status_t (ret);
 }
